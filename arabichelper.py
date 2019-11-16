9bbe8e2d996d8dfb35a1df057c3455e58806305b
Ahmed Khalf <36672196+ahmedkhalf@users.noreply.github.com>
2019-11-16 21:45:31 +0400

    Update arabichelper.py
    Added console_log class to debug, warn, and print error messages to console

--- arabichelper.py
+++ arabichelper.py
@@ -2,6 +2,25 @@ import requests
 from bs4 import BeautifulSoup
 
 
+class console_log:
+    logLevel = 3 # 3: debug, 2: warn, 1: error
+
+    @staticmethod
+    def debug(msg):
+        if console_log.logLevel >= 3:
+            print("[DEBUG]   " + msg)
+    
+    @staticmethod
+    def warn(msg):
+        if console_log.logLevel >= 2:
+            print("[WARNING] " + msg)
+
+    @staticmethod
+    def error(msg):
+        if console_log.logLevel >= 1:
+            print("[ERROR]   " + msg)
+
+
 def get_page_html(url):
     page = requests.get(url)
     if page.status_code < 300:
@@ -13,4 +32,4 @@ def get_soup(page):
     return soup
 
 
-
+console_log.debug("Starting Program!")
